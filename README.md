4627ff85c09d2a90034b65199646ec59e813cc3d
sasha-klimashevich <aleksandra.klimashevich@gmail.com>
2018-05-20 22:43:06 +0300

    Task-1: Command line. File system and streams

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # node-js-course
+
+```
+Example usages:
+
+node dist/index.js --action outputFile  --file data/mocks-data-min.csv
+
+node dist/index.js --action convertToFile  --file data/mocks-data-min.csv
+
+node dist/index.js --action convertFromFile  --file data/mocks-data-min.csv
+
+```
---
+++ src/utils/streams.js
@@ -0,0 +1,87 @@
+import fs from 'fs';
+import csv from 'csv';
+import through from 'through2';
+import { parseArgumentOptions, printHelpMessage, resolveFilePath } from './helpers';
+
+/**
+ * Point 6.b: convert data from process.stdin to upper-cased data on process.stdout
+ */
+function transform() {
+  process.stdin
+    .pipe(through(function (buffer) {
+      this.push(buffer.toString().toUpperCase());
+      process.exit(0);
+    }));
+}
+
+/**
+ * Point 6.c: pipe the given file provided by --file option to process.stdout
+ * @param {String} file
+ */
+function outputFile({ file }) {
+  const resolvedPath = resolveFilePath(file);
+  fs.createReadStream(resolvedPath).pipe(process.stdout);
+}
+
+/**
+ * Outputs data to a result file as json call process.stdout
+ * @param {Object} file
+ * @param {Boolean} saveJson
+ */
+function convert(file, saveJson = false) {
+  const resolvedPath = resolveFilePath(file);
+  const reader = fs.createReadStream(resolvedPath);
+  const newFileName = `${file.slice(0, file.lastIndexOf('.'))}.json`;
+  const logData = saveJson ? fs.createWriteStream(newFileName) : process.stdout;
+  const csvStream = csv.parse().on('data', data => logData.write(`${data.toString()}\n`));
+
+  reader.pipe(csvStream);
+}
+
+/**
+ * Point 6.d: output data using process.stdout
+ * @param {String} file
+ */
+function convertFromFile({ file }) {
+  convert(file);
+}
+
+/**
+ * Point 6.e: output data to a result file as json
+ * @param {String} file
+ */
+function convertToFile({ file }) {
+  convert(file, true);
+}
+
+/**
+ * Calls method according to action
+ * @param {Object} options
+ */
+export function runAction(options) {
+  const methods = {
+    transform,
+    outputFile,
+    convertFromFile,
+    convertToFile,
+  };
+
+  return methods[options.action](options);
+}
+
+/**
+ * Processes user's input value in command line
+ */
+export function processUserInput() {
+  const options = parseArgumentOptions(process.argv);
+
+  if (!options.action && !options.help) {
+    /* Point 3: notify user about wrong input if module is called without arguments */
+    console.error('Error: Invalid input!');
+    printHelpMessage();
+  }
+
+  return options.help
+    ? printHelpMessage()
+    : runAction(options);
+}
